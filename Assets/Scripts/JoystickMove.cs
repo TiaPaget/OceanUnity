12f034b1acf86b11d0ffe1b598226d7a3a9b59e0
Tia <tia.paget@outlook.com>
2024-10-23 16:06:55 +0100

    player, tilebased scene, movement with joystick, collisions, buoyancy, "drift" in water movement.

---
+++ Assets/Scripts/JoystickMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickMove : MonoBehaviour
+{
+    public Joystick movementJoystick;
+    public float playerSpeed;
+    public float dampingFactor = 0.95f; //"slide" effect
+    public float bouyancyForce = 1.0f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (movementJoystick.Direction.y != 0)
+        {
+            rb.velocity = new Vector2(movementJoystick.Direction.x * playerSpeed, movementJoystick.Direction.y * playerSpeed);
+        }
+        else
+        {
+            rb.velocity *= dampingFactor;
+            rb.AddForce(Vector2.up * bouyancyForce);
+        }
+    }
+}
